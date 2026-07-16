29d40814ae9b798bb899c0c843f410a1e0eaf3be
technetech <techne.tech.lab@gmail.com>
2026-07-15 21:04:34 -0700

    mobile adaptation 1

--- src/sections/Formulario.tsx
+++ src/sections/Formulario.tsx
@@ -136,7 +136,7 @@ export default function Formulario() {
                   </div>
                   
                   <div className="space-y-4">
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                       <div>
                         <input
                           type="text"
